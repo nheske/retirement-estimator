81346124c1d18b75c7dccf32a5818c66483711df
nheske <normheske@gmail.com>
2020-12-27 18:15:29 -0800

    cleaned up ints

--- nest-egg.py
+++ nest-egg.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import matplotlib.pyplot as plt
+import statistics
 
 def read_to_list(file_name):
     """Open a file of data in percent, convert to decimal and return a list"""
@@ -18,8 +19,6 @@ def default_input(prompt, default=None):
         return response
 
 
-values = read_to_list("3_mo_TBill_rate_1926-2013_pct.txt")
-print(len(values))
 print("\nNote: Input data should be in percent, not decimal!\n")
 try:
     bonds = read_to_list('10-yr_TBond_returns_1926-2013_pct.txt')
@@ -39,58 +38,58 @@ print("sbc_blend = 40% SP500/50% TBond/10% Cash\n")
 
 print("Press ENTER to take default value shown in [brackets]. \n")
 
-invest_type = default_input("Enter investment type: ( 1. stocks (default), 2. bonds, 3. sb_blend,  4. sbc_blend): \n", 'stocks').lower()
-while invest_type not in investment_type_args:
-    if invest_type == '1':
-      invest_type = 'stocks'
-    elif invest_type == '2':
-      invest_type = 'bonds' 
-    elif invest_type == '3':
-      invest_type = 'sb_blend'    
-    elif invest_type == '4':
-      invest_type = 'sbc_blend'    
-    else:
-      invest_type = input("Invalid investment. Enter investment type " \
-                        "as listed in prompt: ")
-print(invest_type + " chosen")
-start_value = default_input("Input starting value of investments: 2000000 is default\n", '2000000')
-while not start_value.isdigit():
-    start_value = input("Invalid input! Input integer only: ")
+auto = True
+if auto:
+    invest_type = 'sbc_blend'
+    start_value = 2000000
+    withdrawal = 80000
+    min_years = 10
+    most_likely_years = 20
+    max_years = 40
+    total_runs = 50000
+else:
+    invest_type = default_input("Enter investment number 1. SP500 stocks , 2. 10-yr Treasury Bond, 3. 50% SP500/50% TBond,  4. 40% SP500/50% TBond/10% Cash (default): \n", '4').lower()
+    while invest_type not in investment_type_args:
+        if invest_type == '1':
+          invest_type = 'stocks'
+        elif invest_type == '2':
+          invest_type = 'bonds'
+        elif invest_type == '3':
+          invest_type = 'sb_blend'
+        elif invest_type == '4':
+          invest_type = 'sbc_blend'
+        else:
+          invest_type = input("Invalid investment. Enter investment type " \
+                            "as listed in prompt: ")
+    print(invest_type + " chosen")
+    start_value = default_input("Input starting value of investments: [2000000]\n", '2000000')
+    while not start_value.isdigit():
+        start_value = input("Invalid input! Input integer only: ")
 
-withdrawal = default_input("Input annual pre-tax withdrawal  (today's $. $80000 is default): \n", '80000')
-while not withdrawal.isdigit():
-    withdrawal = input("Invalid input! Input integer only: ")
-min_years = default_input("Input minimum years in retirement (18 is default): \n", '18')
-while not min_years.isdigit():
-    min_years = input("Invalid input! Input integer only: ")
+    withdrawal = int(default_input("Input annual pre-tax withdrawal  (today's dollars): [$80000] \n", '80000'))
+    min_years = int(default_input("Input minimum years in retirement: [5] \n", '5'))
+    most_likely_years = int(default_input("Input most-likely years in retirement: [10] \n",'10'))
+    max_years = int(default_input("Input maximum years in retirement: [20] \n", '20'))
+    total_runs = int(default_input("Input number of cases to run: [5000]\n", '5000'))
+    while not total_runs.isdigit():
+        total_runs = input("Invalid input! Input integer only: ")
 
-most_likely_years = default_input("Input most-likely years in retirement (25 is default): \n",
-                                  '25')
-while not most_likely_years.isdigit():
-    most_likely_years = input("Invalid input! Input integer only: ")
+    if not int(min_years) < int(most_likely_years) < int(max_years)  or int(max_years) > 99:
+        print("\nProblem with input years.", file=sys.stderr)
+        print("Requires Min < ML < Max & Max <= 99.", file=sys.stderr)
+        sys.exit(1)
 
-max_years = default_input("Input maximum years in retirement (40 is default): \n", '40')
-while not max_years.isdigit():
-    max_years = input("Invalid input! Input integer only: ")
 
-num_cases = default_input("Input number of cases to run (5000 is default): \n", '5000')
-while not num_cases.isdigit():
-    num_cases = input("Invalid input! Input integer only: ")
+def monte_carlo(returns):
+    lifetime_runs_counter = 0
+    bankrupt_counter = 0
+    lifetime_final_assets = []
 
-if not int(min_years) < int(most_likely_years) < int(max_years)  or int(max_years) > 99:
-    print("\nProblem with input years.", file=sys.stderr)
-    print("Requires Min < ML < Max & Max <= 99.", file=sys.stderr)
-    sys.exit(1)
-
-def montecarlo(returns):
-    case_count = 0
-    bankrupt_count = 0
-    outcome = []
-
-    while case_count < int(num_cases):
-        investments = int(start_value)
+    while lifetime_runs_counter < total_runs:
+        assets_year_end = []
+        assets_value = start_value
         start_year = random.randrange(0, len(returns))
-        duration = int(random.triangular(int(min_years), int(max_years, int(most_likely_years))))
+        duration = round(random.triangular(min_years, max_years, most_likely_years))
         end_year = start_year + duration
         lifespan = [i for i in range(start_year, end_year)]
         bankrupt = 'no'
@@ -108,23 +107,32 @@ def montecarlo(returns):
 
             # don't adjust for inflation first year
             if index == 0:
-                withdraw_infl_adj = int(withdrawal)
+                withdraw_infl_adj = withdrawal
             else:
                 withdraw_infl_adj = int(withdraw_infl_adj * (1 + infl))
-            investments -= withdraw_infl_adj
-            investments = int(investments * (1 + i))
-            if investments <= 0:
+            assets_value -= withdraw_infl_adj
+            assets_value = int(assets_value * (1 + i))
+            assets_year_end.append(assets_value)
+            if assets_value <= 0:
                 bankrupt = 'yes'
                 break
         if bankrupt == 'yes':
-            outcome.append(0)
-            bankrupt_count += 1
+            lifetime_final_assets.append(0)
+            bankrupt_counter += 1
         else:
-            outcome.append(investments)
-            if investments == max(i for i in outcome):
-              print("best outcome so far with $"+investments+"" in case: "+case_count)
-        case_count += 1
-    return outcome, bankrupt_count
+            lifetime_final_assets.append(assets_value)
+            if assets_value == max(i for i in lifetime_final_assets):
+              print("best outcome so far with ${:,.2f}".format(assets_value) +" in case: "+str(lifetime_runs_counter) + " over " + str(duration) + " years")
+ #             print(*assets_year_end)
+              # Annualized Return Formula when given the annual returns for each year of the investment period:
+              # ar = ( ( (1+R1)*(1+R2)*(1+R3)...*(1+Rn) )(1/n) -1) * 100)
+              product = 1
+              for r in lifespan_returns:
+                  product *= (1 + r)
+              annual_return = (product ** (1 / len(lifespan_returns)) - 1) * 100
+              print("annualized return = "+str(round(annual_return, 1)) +"%")
+        lifetime_runs_counter += 1
+    return lifetime_final_assets, bankrupt_counter
 
 def bankrupt_prob(outcome, bankrupt_count):
     """Calculate and return chance of running out of money & other stats."""
@@ -138,15 +146,20 @@ def bankrupt_prob(outcome, bankrupt_count):
           .format(min_years, most_likely_years, max_years))
     print("Number of runs: {:,}\n".format(len(outcome)))
     print("Odds of running out of money: {}%\n".format(odds))
-    print("Average outcome: ${:,}".format(int(sum(outcome) / total)))
+    print("Mean outcome: ${:,}".format(int(sum(outcome) / total)))
+    print("Median outcome:  ${:,}".format(int(statistics.median(outcome))))
     print("Minimum outcome: ${:,}".format(min(i for i in outcome)))
     print("Maximum outcome: ${:,}".format(max(i for i in outcome)))
+
+#TODO calculate annualized return
+#TODO portfolio risk using variance and standard deviations https://codingandfun.com/portfolio-risk-and-returns-python/
+#TODO apply Modern Portfolio Theory https://medium.com/analytics-vidhya/constructing-a-killer-investment-portfolio-with-python-51f4f0d344be
 
     return odds
 
 def main():
     """ Call MCS and bankrupt functions and draw bar chart of results."""
-    outcome, bankrupt_count = montecarlo(investment_type_args[invest_type])
+    outcome, bankrupt_count = monte_carlo(investment_type_args[invest_type])
     odds = bankrupt_prob(outcome, bankrupt_count)
 
     # generate matplotlib bar chart
